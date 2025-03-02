1bf4b6cd3fc3e09c0d6be9469ab2edaeb0566f11
AmitBarshai <amit.br30@gmail.com>
2025-03-02 16:18:03 +0200

    Malking

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +25,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if(id == R.id.action_login){
+            Toast.makeText(this, "you selected login", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.action_register){
+            Toast.makeText(this, "you selected register", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.action_start){
+            Toast.makeText(this, "you selected start", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.new_item) {
+            Intent intent = new Intent(this, NewActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
